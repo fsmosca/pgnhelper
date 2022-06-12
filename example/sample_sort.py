172233cacb925df0fac3819d0479436a889005d0
fsmosca <fsmosca@users.noreply.github.com>
2022-06-12 18:10:19 +0800

    Create sample_sort.py

---
+++ example/sample_sort.py
@@ -0,0 +1,15 @@
+"""
+sample_sort.py
+
+pip install pgnhelper
+"""
+
+from pgnhelper import pgnhelper
+
+a = pgnhelper.PgnHelper(
+    'sort',
+    inpgnfn='mygames.pgn',
+    outpgnfn='out.pgn',
+    sort_tag='eco',
+    sort_direction='hightolow')
+a.start()
