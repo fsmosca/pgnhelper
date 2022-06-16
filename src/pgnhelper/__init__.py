11162165cc060b57932c9a2616ea8c6fd2e93880
fsmosca <fsmosca@users.noreply.github.com>
2022-06-16 23:49:43 +0800

    Add round_robin import

--- src/pgnhelper/__init__.py
+++ src/pgnhelper/__init__.py
@@ -1,5 +1,6 @@
 from pkg_resources import get_distribution
 from pgnhelper.__main__ import main
 from pgnhelper.pgnhelper import PgnHelper
+from pgnhelper.roundrobin import round_robin
 
 __version__ = get_distribution('pgnhelper').version
