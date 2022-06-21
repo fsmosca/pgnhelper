a5b429df9cc0563028ceaf6767cf910913d99e81
fsmosca <fsmosca@users.noreply.github.com>
2022-06-21 20:28:46 +0800

    Update __init__.py

--- src/pgnhelper/__init__.py
+++ src/pgnhelper/__init__.py
@@ -1,6 +1,4 @@
 from pkg_resources import get_distribution
 from pgnhelper.__main__ import main
-from pgnhelper.pgnhelper import PgnHelper
-from pgnhelper.roundrobin import round_robin
 
 __version__ = get_distribution('pgnhelper').version
