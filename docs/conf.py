08e55059a9fc3b4b251b88b46e5e2bde498d0d77
fsmosca <fsmosca@users.noreply.github.com>
2022-06-24 07:29:31 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Ferdinand Mosca'
 author = 'Ferdinand Mosca'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
