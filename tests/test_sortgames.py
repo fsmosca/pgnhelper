c38297caf680a50060133ab5df45af486fe6a7c2
fsmosca <fsmosca@users.noreply.github.com>
2022-06-23 13:30:11 +0800

    Update test_sortgames.py

--- tests/test_sortgames.py
+++ tests/test_sortgames.py
@@ -17,7 +17,7 @@ Command line:
 from pathlib import Path
 
 import chess.pgn
-import pgnhelper.sortgames
+import pgnhelper.sort
 
 
 fn = Path('./tests/data/norway_chess_2022_classical.pgn')
@@ -27,7 +27,7 @@ outfn = Path('./tests/data/out_norway_chess_2022_classical.pgn')
 def test_sortgames_white_lowtohigh():
     """Sort the games on white player from low to high.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, 'white', 'lowtohigh')
+    pgnhelper.sort.sort_games(fn, outfn, 'white', 'lowtohigh')
     white, black = '?', '?'
     with open(outfn, 'r') as f:
         while True:
@@ -44,7 +44,7 @@ def test_sortgames_white_lowtohigh():
 def test_sortgames_white_hightolow():
     """Sort the games on white player from high to low.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, 'white', 'hightolow')
+    pgnhelper.sort.sort_games(fn, outfn, 'white', 'hightolow')
     white, black = '?', '?'
     with open(outfn, 'r') as f:
         while True:
@@ -61,7 +61,7 @@ def test_sortgames_white_hightolow():
 def test_sortgames_black_lowtohigh():
     """Sort the games on black player from low to high.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, 'black', 'lowtohigh')
+    pgnhelper.sort.sort_games(fn, outfn, 'black', 'lowtohigh')
     white, black = '?', '?'
     with open(outfn, 'r') as f:
         while True:
@@ -78,7 +78,7 @@ def test_sortgames_black_lowtohigh():
 def test_sortgames_event():
     """Sort the games on event.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, 'event', 'lowtohigh')
+    pgnhelper.sort.sort_games(fn, outfn, 'event', 'lowtohigh')
     event = '?',
     with open(outfn, 'r') as f:
         while True:
@@ -94,7 +94,7 @@ def test_sortgames_event():
 def test_sortgames_site(sort_tag='site', sort_direction='lowtohigh', tag_value='Stavanger NOR'):
     """Sort the games on sort_tag.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, sort_tag, sort_direction)
+    pgnhelper.sort.sort_games(fn, outfn, sort_tag, sort_direction)
     value = '?',
     with open(outfn, 'r') as f:
         while True:
@@ -110,7 +110,7 @@ def test_sortgames_site(sort_tag='site', sort_direction='lowtohigh', tag_value='
 def test_sortgames_date(sort_tag='date', sort_direction='lowtohigh', tag_value='2022.05.31'):
     """Sort the games on sort_tag.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, sort_tag, sort_direction)
+    pgnhelper.sort.sort_games(fn, outfn, sort_tag, sort_direction)
     value = '?',
     with open(outfn, 'r') as f:
         while True:
@@ -126,7 +126,7 @@ def test_sortgames_date(sort_tag='date', sort_direction='lowtohigh', tag_value='
 def test_sortgames_eco(sort_tag='eco', sort_direction='lowtohigh', tag_value='A07'):
     """Sort the games on sort_tag.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, sort_tag, sort_direction)
+    pgnhelper.sort.sort_games(fn, outfn, sort_tag, sort_direction)
     value = '?',
     with open(outfn, 'r') as f:
         while True:
@@ -142,7 +142,7 @@ def test_sortgames_eco(sort_tag='eco', sort_direction='lowtohigh', tag_value='A0
 def test_sortgames_eco_hightolow(sort_tag='eco', sort_direction='hightolow', tag_value='E94'):
     """Sort the games on sort_tag.
     """
-    pgnhelper.sortgames.sort_games(fn, outfn, sort_tag, sort_direction)
+    pgnhelper.sort.sort_games(fn, outfn, sort_tag, sort_direction)
     value = '?',
     with open(outfn, 'r') as f:
         while True:
